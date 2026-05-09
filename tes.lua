456d74fe232cbe4e193020c1e48ebd5c57ac3546
andhikairzaldi-lab <andhikairzaldi@gmail.com>
2026-05-09 08:41:55 +0700

    Update tes.lua

--- tes.lua
+++ tes.lua
@@ -1,1 +1,19 @@
-print("Halo Ruby! Script dari GitHub berhasil dimuat.")
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.CoreGui
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
+Frame.Position = UDim2.new(0.3, 0, 0.3, 0)
+Frame.Size = UDim2.new(0, 200, 0, 100)
+Frame.Active = true
+Frame.Draggable = true -- Biar jendelanya bisa digeser
+
+TextLabel.Parent = Frame
+TextLabel.Size = UDim2.new(1, 0, 1, 0)
+TextLabel.Text = "Halo Ruby!"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1
+
